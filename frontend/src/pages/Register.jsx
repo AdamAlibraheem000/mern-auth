84c122c3baf53e287b660a5031cc0de13cc6a662
AdamAlibraheem000 <adamalibraheem@hotmail.com>
2022-07-02 22:34:03 -0400

    added home & login screen

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import {useState} from 'react'
 import {FaUser} from 'react-icons/fa'
+import {toast} from 'react-toastify'
 
 function Register() {
     const [formData, setFormData] = useState({
@@ -18,6 +19,17 @@ function Register() {
         }) )
     }
 
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+
+
+
+        if(password !== password2){
+            toast.error('passwords do not match')
+        }
+    }
+
 
 
   return (
@@ -30,10 +42,25 @@ function Register() {
     </section>
 
     <section className="form">
-        <form>
+        <form onSubmit={onSubmit}>
             <div className="form-group">
                 <input type="text"  className="form-control" id='name' name='name' value={name}
-                onChange={onChange} placeholder='Enter your name'/>
+                onChange={onChange} placeholder='Enter your name' required/>
+            </div>
+            <div className="form-group">
+                <input type="email"  className="form-control" id='email' name='email' value={email}
+                onChange={onChange} placeholder='Enter your email' required/>
+            </div>
+            <div className="form-group">
+                <input type="password"  className="form-control" id='password' name='password' value={password}
+                onChange={onChange} placeholder='Enter your password' required/>
+            </div>
+            <div className="form-group">
+                <input type="password"  className="form-control" id='password2' name='password2' value={password2}
+                onChange={onChange} placeholder='Confirm password'required/>
+            </div>
+            <div className="form-group">
+                <button className="btn btn-block">Submit</button>
             </div>
         </form>
     </section>
